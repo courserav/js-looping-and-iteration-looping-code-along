975439d8c6485974d6939038de25b722ed4f22d5
courserav <lvsmlit@gmail.com>
2021-08-01 16:17:25 -0400

    commit tree 6912862a3be939a2fb1f03dc2237a4235893ddb1

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function countDown(number){
+    while (number >= 0){
+        console.log(number)
+        number = --number    
+    }
+}
+
+
+function writeCards(names){
+    names.slice(',')
+    let thanks = []
+    for (let i = 0; i < names.length; i++){
+        thanks[i] = "Thank you, " + names[i] + ", for the wonderful surprise gift!"
+    }
+    return thanks
+}
